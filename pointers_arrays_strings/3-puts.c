2e58df2bb345d3a4e06beaf5e277cfde1a546ba5
maresbarro <zahuistephane40@gmail.com>
2024-06-27 10:28:36 +0000

    po3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,18 +2,18 @@
 
 /**
  * _puts - Write a function that prints a string, followed
- 8* by a new line, to stdout.
+ * by a new line, to stdout.
  * @str: an input string
  * Return: Nothing
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int barro = 0;
 
-	while (str[i] != '\0')
+	while (str[barro] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[barro]);
+		barro++;
 	}
 	_putchar('\n');
 }
