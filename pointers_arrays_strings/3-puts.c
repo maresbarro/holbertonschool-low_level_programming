650e6ac9f90ec6bec6344e2397fdacdc236de8d3
maresbarro <zahuistephane40@gmail.com>
2024-06-27 14:38:32 +0000

    po3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,14 +6,13 @@
  * @str: an input string
  * Return: Nothing
  */
-
 void _puts(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		putchar(str[i]);
+		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
