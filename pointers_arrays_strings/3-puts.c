feb38ea993d3dc2c2ac93f875e0f29f20f62b8df
maresbarro <zahuistephane40@gmail.com>
2024-06-27 10:17:36 +0000

    po3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _puts -Write a function that prints a string,
- *followed by a new line, to stdout.
- *@str: An iput string
- *Return: Nothing
+ * _puts - Write a function that prints a string, followed
+ 8* by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
-
 void _puts(char *str)
 {
 	int i = 0;
