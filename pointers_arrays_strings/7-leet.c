9f2c66b80716be16205b38490360e68bb164b999
maresbarro <zahuistephane40@gmail.com>
2024-07-04 12:58:25 +0000

    poi7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * *leet -priny that encodes a string into 1337.
+ * @src: pointer
+ * Return: src
+ */
+
+char *leet(char *src)
+{
+	int i, j;
+
+	char letters[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (src[i] == letters[j])
+			{
+				src[i] = leet[j];
+
+				break;
+			}
+		}
+	}
+
+	return (src);
+}
+
