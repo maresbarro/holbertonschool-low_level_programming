a548e09dd64f332d1fbe4435ceb6e1503d468d7a
maresbarro <zahuistephane40@gmail.com>
2024-07-11 10:43:59 +0000

    rec0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function that prints a string, followed by a new line.
- * @s: An input string to printing
- * Return: Nothing
+ * _puts_recursion -priny that encodes a string
+ * @s: pointe vers un tabl caract
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
