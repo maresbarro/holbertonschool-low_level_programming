56d801e059486631d8a00d5a898feb93bc5c4d52
maresbarro <zahuistephane40@gmail.com>
2024-07-16 19:40:24 +0000

    fun2

--- function_pointers/main.h
+++ function_pointers/main.h
@@ -2,3 +2,4 @@ void print_name(char *name, void (*f)(char *));
 int (*get_op_func(char *s))(int, int);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
